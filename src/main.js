71987d3cb6ba3feee850dbe2a5a424e04f40a341
Ashwini <pardeshia1998@gmail.com>
2022-01-05 21:43:26 +0530

    b1

--- src/main.js
+++ src/main.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
 
-
 // http://localhost:4000/users
 
+const { selectAllUser } = require("./user");
 
-// req object have all information = when frontend making an request it provides 
-// req = read info
-// res = send infomation 
 app.get("/users", (req, res) => {
-    let obj = {message: "Hello Wolrd"};
-    res.json(obj);
+  const list = await selectAllUser();
+  res.json(list);
 });
 
 app.listen(4000, () => console.log("Server starte"));
